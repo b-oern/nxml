41ed653af9ce09cdee1eb1e917b4f71e7435045a
b-oern <b-oern@users.noreply.github.com>
2023-07-07 17:58:03 +0200

    Update analyse.py

--- nlp/analyse.py
+++ nlp/analyse.py
@@ -3,12 +3,14 @@ from nwebclient import runner
 
 class Toxity(runner.BaseJobExecutor):
     MODULES=['detoxify']
+    text_key = 'text'
     def __init__(self):
         from detoxify import Detoxify
         self.classifier_toxity = Detoxify('multilingual')
     def execute(self, data):
         if isinstance(data, str):
             data = {'text': data}
+        text = data[self.text_key]
         data['toxity'] = self.classifier_toxity.predict(text)
         #{'toxicity': 0.00019621708, 'severe_toxicity': 0.00019254998, 'obscene': 0.0012626372, 'identity_attack': 0.0003226225, 'insult': 0.0008828422, 'threat': 0.00013756882, 'sexual_explicit': 9.029167e-05}
         return data
