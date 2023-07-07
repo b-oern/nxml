b1c47b353c9b59eecca7714711937503031b0aba
b-oern <b-oern@users.noreply.github.com>
2023-07-07 17:55:10 +0200

    Update analyse.py

--- nlp/analyse.py
+++ nlp/analyse.py
@@ -9,7 +9,7 @@ class Toxity(runner.BaseJobExecutor):
     def execute(self, data):
         if isinstance(data, str):
             data = {'text': data}
-        data['toxity'] = classifier_toxity.predict(text)
+        data['toxity'] = self.classifier_toxity.predict(text)
         #{'toxicity': 0.00019621708, 'severe_toxicity': 0.00019254998, 'obscene': 0.0012626372, 'identity_attack': 0.0003226225, 'insult': 0.0008828422, 'threat': 0.00013756882, 'sexual_explicit': 9.029167e-05}
         return data
     
