41bc7ab6665a60f9fb9a9698c38ef195826d2ff3
b-oern <b-oern@users.noreply.github.com>
2023-07-07 14:33:14 +0200

    Update analyse.py

--- nlp/analyse.py
+++ nlp/analyse.py
@@ -1,12 +1,19 @@
 
-
-
-# pip install detoxify
-from detoxify import Detoxify
-classifier_toxity = Detoxify('multilingual')
-
 from langdetect import detect
 from nwebclient import runner
+
+class Toxity(runner.BaseJobExecutor):
+    MODULES=['detoxify']
+    def __init__(self):
+        from detoxify import Detoxify
+        self.classifier_toxity = Detoxify('multilingual')
+    def execute(self, data):
+        if isinstance(data, str):
+            data = {'text': data}
+        data['toxity'] = classifier_toxity.predict(text)
+        #{'toxicity': 0.00019621708, 'severe_toxicity': 0.00019254998, 'obscene': 0.0012626372, 'identity_attack': 0.0003226225, 'insult': 0.0008828422, 'threat': 0.00013756882, 'sexual_explicit': 9.029167e-05}
+        return data
+    
 
 class Nltk(runner.BaseJobExecutor):
     MODULES=['nltk']
@@ -83,8 +90,6 @@ def analyse_textblob(data, text_key = 'text'):
 
 def analyse_text(data, text_key = 'text'):
     text = data[text_key]
-    data['toxity'] = classifier_toxity.predict(text)
-    #{'toxicity': 0.00019621708, 'severe_toxicity': 0.00019254998, 'obscene': 0.0012626372, 'identity_attack': 0.0003226225, 'insult': 0.0008828422, 'threat': 0.00013756882, 'sexual_explicit': 9.029167e-05}
     data['lang'] = detect(text)
     data = analyse_textblob(data)
     return data
