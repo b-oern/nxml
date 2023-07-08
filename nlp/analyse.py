40e52593046f1212ed5c991ea9ebf6f41d5e4a20
b-oern <b-oern@users.noreply.github.com>
2023-07-08 16:40:39 +0200

    Update analyse.py

--- nlp/analyse.py
+++ nlp/analyse.py
@@ -12,7 +12,8 @@ class Toxity(runner.BaseJobExecutor):
         if isinstance(data, str):
             data = {'text': data}
         text = data[self.text_key]
-        data['toxity'] = self.classifier_toxity.predict(text)
+        tv =self.classifier_toxity.predict(text)
+        data['toxity'] = dict(map(lambda kv: (kv[0], float(kv[1])), tv.iteritems()))
         #{'toxicity': 0.00019621708, 'severe_toxicity': 0.00019254998, 'obscene': 0.0012626372, 'identity_attack': 0.0003226225, 'insult': 0.0008828422, 'threat': 0.00013756882, 'sexual_explicit': 9.029167e-05}
         data['success'] = True
         return data
