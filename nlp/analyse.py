66c7b7431316185fa47f01af7c716942b6ce82f2
b-oern <b-oern@users.noreply.github.com>
2023-07-09 10:30:55 +0200

    Update analyse.py

--- nlp/analyse.py
+++ nlp/analyse.py
@@ -132,7 +132,10 @@ class NsfwDetector(ImageExecutor):
     def executeImage(self, image, data):
         from nsfw_detector import predict
         image_preds = predict.classify(self.model, self.image_filename())
-        data['nsfw_detector'] = image_preds
+        data['nsfw_detector'] = image_preds.values()[0]
+        data['neutral'] = image_preds.values()[0]['neutral']
+        data['porn'] = image_preds.values()[0]['porn']
+        data['sexy'] = image_preds.values()[0]['sexy']
         return data
     
 
