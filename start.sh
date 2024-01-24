a52a9948495356e9dff1f7f46da73358a55df8bb
b-oern <b-oern@users.noreply.github.com>
2024-01-24 16:57:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pip install numpy nwebclient transformers requests nltk flair
+pip install numpy nwebclient transformers requests nltk flair detoxify
 python3 -m nwebclient.runner --rest --executor nxml.analyse:NlpPipeline
