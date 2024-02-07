bf172ac8e0b401d7bfe3ddaddc792737d54a047a
b-oern <b-oern@users.noreply.github.com>
2024-02-07 11:16:45 +0100

    Abhängigkeiten für age_and_gender

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 pip install numpy nwebclient transformers requests nltk flair detoxify textblob textblob_de langchain sentence_transformers
 
+apt update
+apt install -y wget git curl libjpeg-dev libpng-dev python3-dev cmake gcc libx11-dev
 git clone https://github.com/b-oern/age-and-gender.git
 cd age-and-gender
 pip install .
