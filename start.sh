767ef0ac84f0525b3e408b6f2abd398fa36305be
b-oern <b-oern@users.noreply.github.com>
2024-04-09 11:37:36 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-pip install numpy nwebclient transformers requests nltk flair detoxify textblob textblob_de langchain sentence_transformers pillow
+python3 -m pip install numpy nwebclient transformers requests nltk flair detoxify textblob textblob_de langchain sentence_transformers pillow
 
 apt update
 apt install -y wget git curl libjpeg-dev libpng-dev python3-dev cmake gcc libx11-dev
 git clone https://github.com/b-oern/age-and-gender.git
 cd age-and-gender
-pip install .
+python3 -m pip install .
 cd ..
 
 python3 -m nwebclient.runner --rest --executor nxml.analyse:AnalyseMain
