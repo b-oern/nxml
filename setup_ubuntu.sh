8f08bef11e3722c0e6ef1e7f945d4e004fa0426d
b-oern <b-oern@users.noreply.github.com>
2024-05-02 15:26:12 +0200

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,6 +2,8 @@
 apt update
 apt install -y python3 python3-pip wget git curl
 
+rm /usr/lib/python3.*/EXTERNALLY-MANAGED
+
 # python liegt auf python3
 
 pip install numpy nwebclient transformers requests nltk flask
