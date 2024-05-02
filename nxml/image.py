384864039962dcefdb7e8f3e28e282c2c0b4e53d
Björn <bjoern.salgert@hs-duesseldorf.de>
2024-05-02 13:36:48 +0200

    image_similarity

--- nxml/image.py
+++ nxml/image.py
@@ -14,9 +14,12 @@ class ImageSimilarity(r.ImageExecutor):
     """
 
     MODULES = ['opencv-python', 'scikit-image']
+    NS = 'image_similarity'
     type = 'image_similarity'
 
-    def compareImageSSIM(self, image_a, image_b):
+    threshold = 0.5
+
+    def compareImagesSSIM(self, image_a, image_b):
         import cv2
         from skimage import metrics
         import numpy
@@ -35,6 +38,35 @@ class ImageSimilarity(r.ImageExecutor):
         #print(f"SSIM Score: ", round(ssim_score[0], 2))
         return round(ssim_score[0], 2)
 
+    def compareImagesCLIP(self, image_a, image_b):
+        # !pip install git+https://github.com/openai/CLIP.git
+        # !pip install open_clip_torch
+        # !pip install sentence_transformers
+        import torch
+        import open_clip
+        import cv2
+        from sentence_transformers import util
+        from PIL import Image
+        # image processing model
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-16-plus-240', pretrained="laion400m_e32")
+        model.to(device)
+
+        def imageEncoder(img):
+            img1 = preprocess(img).unsqueeze(0).to(device)
+            img1 = model.encode_image(img1)
+            return img1
+
+        def generateScore(image1, image2):
+            img1 = imageEncoder(image1)
+            img2 = imageEncoder(image2)
+            cos_scores = util.pytorch_cos_sim(img1, img2)
+            score = round(float(cos_scores[0][0]) * 100, 2)
+            return score
+
+        #print(f"similarity Score: ", round(generateScore(image_a, image_b), 2))
+        return round(generateScore(image_a, image_b), 2)/100
+
     def searchSimilar(self, image, data):
         n = NWebClient(None)
         result = {'images': [], 'nweb': n.url()}
@@ -47,7 +79,7 @@ class ImageSimilarity(r.ImageExecutor):
         for d in docs:
             if d.is_image():
                 img_b = d.as_image()
-                d.similarity = self.compareImageSSIM(image, img_b)
+                d.similarity = self.compareImagesSSIM(image, img_b)
                 self.info("Similarity: "+str(d.similarity))
         docs.sort(key=lambda x: x.similarity, reverse=True)
         i = 1
@@ -59,6 +91,29 @@ class ImageSimilarity(r.ImageExecutor):
                 break
         return result
 
+    def index(self, data):
+        self.n = NWebClient(None)
+        docs = self.n.docs('kind=image&no_meta='+self.NS+'_cfg'+'.max_id&limit=10')
+        for d in docs:
+            self.do_doc(d)
+        return {'from': 'index()'}
+
+    def get_docs(self):
+        docs = self.n.docs('kind=image&limit=1000000')
+        return docs
+
+    def do_doc(self, doc):
+        # Ein Bild mit allen anderen vergleichen
+        img_a = doc.as_image()
+        max_id = 0
+        for b in self.get_docs():
+            max_id = max(max_id, b.id())
+            score = self.compareImagesSSIM(img_a, b.as_image())
+            if score > self.threshold:
+                doc.setMetaValue(self.NS, str(b.id()), score)
+        doc.setMetaValue(self.NS+'_cfg', 'max_id', max_id)
+        self.info("Done Doc.")
+
     def executeImage(self, image, data):
         b = self.get_image('b', data)
         if b is not None:
@@ -66,6 +121,8 @@ class ImageSimilarity(r.ImageExecutor):
             return {'success': True, 'score': score}
         if 'search' in data:
             return self.searchSimilar(image, data)
+        if 'index' in data:
+            return self.index(data)
         else:
             return {}
 
